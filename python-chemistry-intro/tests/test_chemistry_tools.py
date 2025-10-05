a70d11762d28fa21c5905e6bd6edfbf4c166b6d0
Nicola Schneider <schneidnicola@gmail.com>
2025-10-05 14:58:13 +0200

    feat: Added molecular weight calculator and tests for this function. Added new script explore formula.

--- python-chemistry-intro/tests/test_chemistry_tools.py
+++ python-chemistry-intro/tests/test_chemistry_tools.py
@@ -1,4 +1,4 @@
-from dsa103.chemistry_tools import calculate_molecular_formula
+from dsa103.chemistry_tools import calculate_molecular_formula, calculate_molecular_weight
 
 
 def test_calculate_molecular_formula():
@@ -9,3 +9,14 @@ def test_calculate_molecular_formula():
 
     formula_caffeine = calculate_molecular_formula("CN1C=NC2=C1C(=O)N(C(=O)N2C)C")
     assert formula_caffeine == "C8H10N4O2"
+
+def test_calculate_molecular_weight():
+    """Tests for calculate_molecular_weight."""
+
+    weight_water = calculate_molecular_weight("O")
+    # Molecular weight of H2O is about 18.0106
+    assert round(weight_water, 4) == 18.0106
+
+    weight_caffeine = calculate_molecular_weight("CN1C=NC2=C1C(=O)N(C(=O)N2C)C")
+    # Molecular weight of C8H10N4O2 is about 194.0804
+    assert round(weight_caffeine, 4) == 194.0804
